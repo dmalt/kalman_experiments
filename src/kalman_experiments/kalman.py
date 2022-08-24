702fc28cada28201ca830a3f1ff75ad36abf194c
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-08-25 01:15:09 +0300

    refactor kalman code

--- src/kalman_experiments/kalman.py
+++ src/kalman_experiments/kalman.py
@@ -1,9 +1,17 @@
+from cmath import exp
+from typing import Any, NamedTuple
+
 import numpy as np
+import numpy.typing as npt
 
-Arr = np.ndarray
+from .complex import complex2mat
+
+Cov = npt.NDArray[np.floating[Any]]  # of shape(n, n)
+Vec = npt.NDArray[np.floating[Any]]  # of shape(n, 1)
+Mat = npt.NDArray[np.floating[Any]]  # of shape(n, m)
 
 
-class ColoredMeasurementNoiseKF:
+class ColoredNoiseKF:
     """
     'Alternative approach' implementation for KF with colored noise from [1]
 
@@ -34,142 +42,114 @@ class ColoredMeasurementNoiseKF:
 
     """
 
-    def __init__(self, n_x: int, n_z: int, Phi: Arr, Q: Arr, H: Arr, Psi: Arr, R: Arr):
+    def __init__(self, n_x: int, n_z: int, Phi: Mat, Q: Cov, H: Mat, Psi: Mat, R: Cov):
         self.Phi = Phi
         self.Q = Q
         self.H = H
         self.Psi = Psi
         self.R = R
 
-        self.x_posterior = np.zeros(n_x)[:, np.newaxis]
-        self.P_posterior = np.zeros((n_x, n_x))
+        self.x = np.zeros((n_x, 1))  # posterior state (after update)
+        self.P = np.zeros((n_x, n_x))  # posterior state covariance (after update)
 
-        self.ym1 = np.zeros(n_z)
+        self.y_prev = np.zeros((n_z, 1))
 
-    def predict(self):
-        x_prior = self.Phi @ self.x_posterior  # eq. (26) from [1]
-        P_prior = self.Phi @ self.P_posterior @ self.Phi.T + self.Q  # eq. (27) from [1]
-        return x_prior, P_prior
+    def predict(self) -> tuple[Vec, Cov]:
+        x_ = self.Phi @ self.x  # eq. (26) from [1]
+        P_ = self.Phi @ self.P @ self.Phi.T + self.Q  # eq. (27) from [1]
+        return x_, P_
 
-    def update(self, y):
-        x_prior, P_prior = self.predict()
+    def update(self, y: Vec, x_: Vec, P_: Cov) -> tuple[Vec, Cov]:
+        A = self.Psi @ self.H
+        B = self.H @ self.Phi
+        P, H, R = self.P, self.H, self.R
 
-        z = y - self.Psi * self.ym1  # eq. (35) from [1]
-        n = z - self.H @ x_prior + self.Psi * self.H @ self.x_posterior  # eq. (37) from [1]
-        # eq. (38) from [1]
-        Sigma = (
-            self.H @ P_prior @ self.H.T
-            + self.Psi @ self.H @ self.P_posterior @ self.H.T @ self.Psi.T
-            + self.R
-            - self.H @ self.Phi @ self.P_posterior @ self.H.T @ self.Psi.T
-            - self.Psi @ self.H @ self.P_posterior @ self.Phi.T @ self.H.T
-        )
-        # eq. (39) from [1]
-        Pxn = P_prior @ self.H.T - self.Phi @ self.P_posterior @ self.H.T @ self.Psi.T
+        z = y - self.Psi @ self.y_prev  # eq. (35) from [1]
+        n = z - self.H @ x_ + A @ self.x  # eq. (37) from [1]
+        Sigma = H @ P_ @ H.T + A @ P @ A.T + R - B @ P @ A.T - A @ P @ B.T  # eq. (38) from [1]
+        Pxn = P_ @ self.H.T - self.Phi @ P @ A.T  # eq. (39) from [1]
 
         K = Pxn / Sigma  # eq. (40) from [1]
-        self.x_posterior = x_prior + K * n  # eq. (41) from [1]
-        # eq. (42) from [1]
-        self.P_posterior = P_prior - K * Sigma @ K.T
-        self.ym1 = y
-        return x_prior, self.x_posterior, n, K, Sigma, self.P_posterior
+        self.x = x_ + K * n  # eq. (41) from [1]
+        self.P = P_ - K * Sigma @ K.T  # eq. (42) from [1]
+        self.y_prev = y
+        return self.x, self.P
+
+    def update_no_meas(self, x_: Vec, P_: Cov):
+        """Update step when the measurement is missing"""
+        self.x = x_
+        self.P = P_
+        self.y_prev = self.H @ x_
+        return x_, P_
+
+    def step(self, y: Vec | None) -> tuple[Vec, Cov]:
+        x_, P_ = self.predict()
+        return self.update(y, x_, P_) if y is not None else self.update_no_meas(x_, P_)
 
 
-def plv(x1, x2, ma_len_samp):
-    x1 /= np.abs(x1)
-    x2 /= np.abs(x2)
-    prod = np.conj(x1) * x2
-
-    ma_kernel = np.ones(ma_len_samp) / ma_len_samp
-    return np.convolve(prod, ma_kernel, mode="same"), prod[: len(prod) // 2].mean()
+class Gaussian(NamedTuple):
+    mu: Vec
+    Sigma: Cov
 
 
-def env_cor(x1, x2, ma_len_samp):
-    x1 = np.abs(x1)
-    x1 -= x1.mean()
-    x1 /= x1.std()
-    x2 = np.abs(x2)
-    x2 -= x2.mean()
-    x2 /= x2.std()
-    prod = x1 * x2
+class Colored1DMatsudaKF:
+    """
+    Single oscillation - single measurement Kalman filter with colored noise
 
-    ma_kernel = np.ones(ma_len_samp) / ma_len_samp
-    return np.convolve(prod, ma_kernel, mode="same"), prod.mean()
+    Using Matsuda's model for oscillation prediction, see [1], and AR(1) to
+    make account for 1/f^a measurement noise, see [2]. Wraps ColoredNoiseKF to
+    avoid trouble with properly arranging matrix and vector shapes.
 
+    Parameters
+    ----------
+    A : float
+        A in Matsuda's step equation: x_next = A * exp(2 * pi * i * f / sr) * x + n
+    f : float
+        Oscillation frequency; f in Matsuda's step equation:
+        x_next = A * exp(2 * pi * i * f / sr) * x + n
+    sr : float
+        Sampling rate
+    q_sigma : float
+        Standard deviation of model's driving noise (std(n) in the formula above),
+        see eq. (1) in [2] and the explanation below
+    psi : float
+        Coefficient of the AR(1) process modelling 1/f^a colored noise;
+        see eq. (3) in [2]; -0.5 corresponds to 1/f noise, 0 -- to white noise
+    r_sigma : float
+        Driving white noise covariance matrix for the noise AR model
+        (cov for e_{k-1} in eq. (3) in [2])
 
-if __name__ == "__main__":
-    from gen_ar_noise import ArNoise
-    from models import SingleRhythmModel, complex2mat, vec2complex
-    from real_noise import prepare_real_noise
+    References
+    ----------
+    .. [1] Matsuda, Takeru, and Fumiyasu Komaki. “Time Series Decomposition
+    into Oscillation Components and Phase Estimation.” Neural Computation 29,
+    no. 2 (February 2017): 332–67. https://doi.org/10.1162/NECO_a_00916.
 
-    order = 1
-    alpha = 1
-    sigma_noise = 4
-    meas_noise = ArNoise(y0=np.random.rand(order), alpha=alpha, order=order, sigma=sigma_noise)
-    # meas_noise = prepare_real_noise(raw_path="./sub-01_ses-session2_task-eyesopen_eeg.vhdr")
-    sr = 125
-    sigma_data = 1
-    model = SingleRhythmModel(freq=10, A=0.99, sigma=sigma_data, sr=sr, meas_noise_ar=meas_noise)
+    .. [2] Chang, G. "On kalman filter for linear system with colored
+    measurement noise". J Geod 88, 1163–1170, 2014
+    https://doi.org/10.1007/s00190-014-0751-7
 
-    kf = ColoredMeasurementNoiseKF(
-        n_x=2,
-        n_z=1,
-        Phi=complex2mat(model.Phi),
-        Q=np.eye(N=2) * sigma_data**2,
-        H=np.array([1, 0])[np.newaxis, :],
-        # Psi=np.array([meas_noise.a[0]])[:, np.newaxis],
-        Psi=np.array([-0.5])[:, np.newaxis],
-        R=np.array([(sigma_noise) ** 2])[:, np.newaxis],
-    )
-    gt_states = []
-    kf_states = []
-    meas = []
-    for i in range(1000):
-        y = model.step()
-        gt_states.append(model.x)
-        meas.append(y)
-        kf_states.append(vec2complex(kf.update(y)[1][:, 0]))
-    for i in range(1000):
-        y = model.step()
-        gt_states.append(model.x)
-        pred = kf.predict()
-        kf.x_posterior = pred[0]
-        kf.P_posterior = pred[1]
-        kf_states.append(vec2complex(pred[0][:, 0]))
-        meas.append(y)
-    gt_states = np.array(gt_states)
-    kf_states = np.array(kf_states)
-    meas = np.array(meas)
+    """
 
-    from cfir import CFIRBandDetector
+    def __init__(self, A: float, f: float, sr: float, q_sigma: float, psi: float, r_sigma: float):
+        Phi = complex2mat(A * exp(2 * np.pi * f / sr * 1j))
+        Q = np.eye(2) * q_sigma**2
+        H = np.array([[1, 0]])
+        Psi = np.array([[psi]])
+        R = np.array([[r_sigma**2]])
+        self.KF = ColoredNoiseKF(n_x=2, n_z=1, Phi=Phi, Q=Q, H=H, Psi=Psi, R=R)
 
-    cfir = CFIRBandDetector([8, 12], sr, delay=0)
-    cfir_states = cfir.apply(meas)
+    def predict(self) -> Gaussian:
+        return Gaussian(*self.KF.predict())
 
-    import matplotlib.pyplot as plt
+    def update(self, y: float, X_: Gaussian) -> Gaussian:
+        y_arr = np.array([[y]])
+        return Gaussian(*self.KF.update(y=y_arr, x_=X_.mu, P_=X_.Sigma))
 
-    lim = 2000
-    t = np.arange(lim) / sr - lim / 2 / sr
-    f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-    ax1.plot(t, np.real(cfir_states[:lim]))
-    ax1.plot(t, np.real(kf_states[:lim]))
-    ax1.plot(t, meas[:lim], alpha=0.3)
-    ax1.plot(t, np.real(gt_states[:lim]), "--")
-    ax1.legend(["cfir state (Re)", "kalman state (Re)", "measurements", "ground truth state (Re)"])
-    ax1.grid()
+    def update_no_meas(self, X_: Gaussian) -> Gaussian:
+        """Update step when the measurement is missing"""
+        return Gaussian(*self.KF.update_no_meas(x_=X_.mu, P_=X_.Sigma))
 
-    # ax1.grid()
-    plt.xlabel("Time, sec")
-
-    plv_win_kf, plv_tot_kf = plv(gt_states.copy(), kf_states.copy(), int(0.5 * sr))
-    plv_win_cfir, plv_tot_cfir = plv(gt_states.copy(), cfir_states.copy(), int(0.5 * sr))
-    envcor_win_kf, envcor_tot_kf = env_cor(gt_states.copy(), kf_states.copy(), int(0.5 * sr))
-    envcor_win_cfir, envcor_tot_cfir = env_cor(gt_states.copy(), cfir_states.copy(), int(0.5 * sr))
-    print(f"{np.abs(plv_tot_kf)=}, {np.abs(plv_tot_cfir)=}")
-    ax2.plot(t, np.abs(plv_win_cfir), linewidth=2)
-    ax2.plot(t, np.abs(plv_win_kf), linewidth=2)
-    # ax2.plot(t, envcor_win_kf, linewidth=2)
-    # ax2.plot(t, envcor_win_cfir, linewidth=2)
-    ax2.legend(["plv(gt, cfir)", "plv(gt, kf)"])  # , "envcor(gt, kf)", "envcor(gt, cfir)"])
-    plt.grid()
-    plt.show()
+    def step(self, y: float | None) -> Gaussian:
+        X_ = self.predict()
+        return self.update_no_meas(X_) if y is None else self.update(y, X_)
