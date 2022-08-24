80b83eb32bd89cec5e7888587ac882dffac78d3d
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-08-25 01:15:19 +0300

    fix complex utils to use proper dimensions

--- src/kalman_experiments/complex.py
+++ src/kalman_experiments/complex.py
@@ -1,25 +1,28 @@
+"""Complex numbers manipulation utilities"""
+
 import random
+from typing import Any
 
 import numpy as np
 import numpy.typing as npt
 
-Vec2 = npt.NDArray  # array of shape(2,)
-Mat2 = npt.NDArray  # array of shape(2, 2)
+Vec2 = npt.NDArray[np.floating[Any]]  # array of shape(2, 1)
+Mat2 = npt.NDArray[np.floating[Any]]  # array of shape(2, 2)
 
 
 def complex_randn() -> complex:
-    """Generate random complex number with real and imag parts sampled from N(0, 1)"""
+    """Generate random complex number with Re and Im sampled from N(0, 1)"""
     return random.gauss(0, 1) + 1j * random.gauss(0, 1)
 
 
 def complex2vec(z: complex) -> Vec2:
     """Convert complex number to 2d vector"""
-    return np.array([z.real, z.imag])
+    return np.array([[z.real], [z.imag]])
 
 
 def vec2complex(v: Vec2) -> complex:
     """Convert 2d vector to a complex number"""
-    return v[0] + 1j * v[1]
+    return v[0, 0] + 1j * v[1, 0]
 
 
 def complex2mat(z: complex) -> Mat2:
