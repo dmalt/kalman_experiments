a834fc9d9db1ba5f7f129e69b082fd06d62f8c82
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-08-25 10:53:40 +0300

    merge simulated and real noise modules

--- src/kalman_experiments/noise.py
+++ src/kalman_experiments/noise.py
@@ -1,4 +1,5 @@
 import numpy as np
+from mne.io import read_raw_brainvision
 
 
 class ArNoise:
@@ -38,6 +39,36 @@ class ArNoise:
         y_next = - self.a @ self.y + np.random.randn() * self.sigma
         self.y = np.concatenate([[y_next], self.y[:-1]])  # type: ignore
         return float(y_next)
+
+
+class RealNoise:
+    def __init__(self, single_channel_eeg: np.ndarray):
+        self.single_channel_eeg = single_channel_eeg
+        self.ind = 0
+
+    def step(self) -> float:
+        n_samp = len(self.single_channel_eeg)
+        if self.ind >= len(self.single_channel_eeg):
+            raise IndexError(f"Index {self.ind} is out of bounds for data of length {n_samp}")
+        self.ind += 1
+        return self.single_channel_eeg[self.ind]
+
+
+def prepare_real_noise(
+    raw_path: str, sigma: float = 1, minsamp: int = 0, maxsamp: int | None = None
+) -> tuple[RealNoise, float]:
+    raw = read_raw_brainvision(raw_path, preload=True)
+    raw.pick_channels(["FC2"])
+    raw.crop(tmax=244)
+    raw.filter(l_freq=0.1, h_freq=None)
+
+    data = np.squeeze(raw.get_data())
+    data /= data.std()
+    data -= data.mean()
+    print("dm=", data.mean())
+    data *= sigma
+    crop = slice(minsamp, maxsamp)
+    return RealNoise(data[crop]), raw.info["sfreq"]
 
 
 if __name__ == "__main__":
