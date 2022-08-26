55eb7075d8044b1d1d2fb61aac9485e167da2943
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-08-26 14:14:28 +0300

    update code

--- src/kalman_experiments/models.py
+++ src/kalman_experiments/models.py
@@ -31,3 +31,16 @@ class SingleRhythmModel:
         """Update model state and generate measurement"""
         self.x = self.Phi * self.x + complex_randn() * self.sigma
         return self.x.real + self.meas_noise.step()
+
+
+class ModelAdapter:
+    def __init__(self, model: SingleRhythmModel):
+        self.model = model
+
+    def create_states_and_meas(self, n_samp: int):
+        states = np.zeros(n_samp, dtype=complex)
+        meas = np.zeros(n_samp)
+        for i in range(n_samp):
+            meas[i] = self.model.step()
+            states[i] = self.model.x
+        return states, meas
