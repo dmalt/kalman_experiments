4a17ca0e875b9e74b535971d29fe21dae0e91d2d
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-10-26 20:08:28 +0300

    improve comment for numpy types

--- src/kalman_experiments/numpy_types.py
+++ src/kalman_experiments/numpy_types.py
@@ -3,12 +3,12 @@ from typing import Any
 import numpy as np
 import numpy.typing as npt
 
-Vec1D = npt.NDArray[np.floating[Any]]  # of shape(n,)
-Cov = npt.NDArray[np.floating[Any]]  # of shape(n, n)
-Vec = npt.NDArray[np.floating[Any]]  # of shape(n, 1)
-Mat = npt.NDArray[np.floating[Any]]  # of shape(n, m)
+Vec1D = npt.NDArray[np.floating[Any]]  # one-dimensional vector of shape(n,)
+Cov = npt.NDArray[np.floating[Any]]  # covariance matrix of shape(n, n)
+Vec = npt.NDArray[np.floating[Any]]  # column vector of shape(n, 1)
+Mat = npt.NDArray[np.floating[Any]]  # matrix of shape(n, m)
 
-Vec2 = npt.NDArray[np.floating[Any]]  # of shape(2, 1)
-Mat2 = npt.NDArray[np.floating[Any]]  # of shape(2, 2)
+Vec2 = npt.NDArray[np.floating[Any]]  # 2-dimensional vector of shape(2, 1)
+Mat2 = npt.NDArray[np.floating[Any]]  # 2-dimensional vector of shape(2, 2)
 
-Timeseries = npt.NDArray[np.number]  # of shape(n_samp,)
+Timeseries = npt.NDArray[np.number]  # one-dimensional timeseries vector of shape(n_samp,)
